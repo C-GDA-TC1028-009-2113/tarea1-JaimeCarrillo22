3ed87bc603c1f47e3b189bb09b11f697ab116026
JaimeCarrillo22 <A01642164@tec.mx>
2021-08-22 17:16:01 +0000

    Tarea1

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    Edad= int(input("Dame tu edad: "))
+    Año_actual= int(input("Dame el año actual: "))
+    resta= Año_actual-Edad
+    suma= resta+100
+    print("Cumplirás 100 años en el año: " + str(suma))
 
 
 
